9d649e2f51b59dc7845230a3388c15e1a89c4ee1
anacarolina1 <ana_karolopes@hotmail.com>
2016-07-06 23:07:57 -0300

    Add html page

--- ep2/ep2Site/views.py
+++ ep2/ep2Site/views.py
@@ -4,6 +4,8 @@ import twitter
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.views.generic import View
+from models import Review
 
 
 def home(request):
@@ -36,3 +38,6 @@ def present_output(form):
 
 '''
 
+def music(request):
+	form = ReviewForm
+	return render(request, 'ep2Site/music.html', {'form': form})
